e8973eee3efd94485b53a2b953c1d02e91604fe2
Chris Lucian <Chris@DeadlyApps.com>
2016-01-28 22:50:47 -0800

    Before and After Text added. Text Area is now smaller

--- Controllers/SpeedReaderController.py
+++ Controllers/SpeedReaderController.py
@@ -7,5 +7,8 @@ from Frames.MainFrame import MainFrame
 class SpeedReaderController(Tk):
     def __init__(self):
         Tk.__init__(self)
+        self.title("Speed Reader")
         main_frame = MainFrame()
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
         main_frame.grid(padx=50, pady=50, sticky=(N, S, E, W))
--- Frames/MainFrame.py
+++ Frames/MainFrame.py
@@ -1,6 +1,6 @@
 import thread
 import ttk
-from Tkconstants import END, N, S, E, W, NORMAL, DISABLED
+from Tkconstants import END, N, S, E, W, NORMAL, DISABLED, RIGHT, CENTER
 from Tkinter import Text
 
 import pyttsx
@@ -16,60 +16,89 @@ class MainFrame(ttk.Frame):
         self.build_frame_content(kw)
 
     def build_frame_content(self, kw):
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=0)
+        self.grid_columnconfigure(3, weight=1)
+
         row_index = 0
-        self.current_word_label = ttk.Label(self, font=("Georgia", "52"))
-        self.current_word_label.grid(row=row_index, columnspan=2)
+        self.grid_rowconfigure(row_index, weight=1)
+        self.title = ttk.Label(self, font=("Georgia", "80"), justify=RIGHT, text="Speed Reader", anchor=CENTER)
+        self.title.grid(row=row_index, column=1,columnspan=2, sticky=(N, W, E),pady=15)
+        row_index += 1
+
+        self.spoken_words = ttk.Label(self, font=("Georgia", "20"), justify=RIGHT, anchor=E)
+        self.spoken_words.grid(row=row_index, column=1, columnspan=2, sticky=(W,E))
+        row_index += 1
+
+        self.current_word_label = ttk.Label(self, font=("Georgia", "120"), anchor=CENTER)
+        self.current_word_label.grid(row=row_index, column=1, columnspan=2, sticky=(W,E))
+        row_index += 1
+
+
+        self.next_words = ttk.Label(self, font=("Georgia", "20"), anchor=W)
+        self.next_words.grid(row=row_index, column=1, columnspan=2, sticky=(W, E))
+
         row_index += 1
 
         self.speed_label = ttk.Label(self, text="Speed: ")
-        self.speed_label.grid(row=row_index, column=0, pady=10)
+        self.speed_label.grid(row=row_index, column=1, pady=10)
         self.speed_entry = ttk.Entry(self)
         self.speed_entry.insert(0, "500")
-        self.speed_entry.grid(row=row_index, column=1, pady=10)
+        self.speed_entry.grid(row=row_index, column=2, pady=10)
         row_index += 1
 
-        self.text_area = Text(self, height=50, width=190)
+        self.grid_rowconfigure(row_index, weight=1)
+        self.text_area = Text(self, height=5, width=1, font=("Georgia", "25"))
         self.text_area.insert(END, '')
         self.text_area.tag_config(TAG_CURRENT_WORD, foreground="red")
-        self.text_area.grid(row=row_index, columnspan=2, sticky=(N, S, E, W))
+        self.text_area.grid(row=row_index, column=0, columnspan=4, sticky=(N, S, E, W))
         row_index += 1
 
         self.speak_button = ttk.Button(self, text="Speak")
-        self.speak_button.grid(row=row_index, column=0, pady=10)
+        self.speak_button.grid(row=row_index, column=1, pady=10)
         self.speak_button['state'] = NORMAL
         self.speak_button.bind("<Button-1>", self.speak)
 
-        # self.stop_button = ttk.Button(self, text="Stop")
-        # self.stop_button.grid(row=row_index, column=1, pady=10)
-        # self.stop_button['state'] = DISABLED
-        # self.stop_button.bind("<Button-1>", self.stop)
+        self.stop_button = ttk.Button(self, text="Stop")
+        self.stop_button.grid(row=row_index, column=2, pady=10)
+        self.stop_button['state'] = DISABLED
+        self.stop_button.bind("<Button-1>", self.stop)
 
-    # def stop(self, event):
-    #     if self.stop_button['state'].__str__() == NORMAL:
-    #         self.engine.stop()
-    #         self.speak_button['state'] = NORMAL
-    #         self.stop_button['state'] = DISABLED
+    def stop(self, event):
+        if self.stop_button['state'].__str__() == NORMAL:
+            self.engine.stop()
+            self.speak_button['state'] = NORMAL
+            self.stop_button['state'] = DISABLED
 
     def onStart(self, name):
         self.speak_button['state'] = DISABLED
-        # self.stop_button['state'] = NORMAL
+        self.stop_button['state'] = NORMAL
         print("onStart")
 
     def onWord(self, name, location, length):
+        left_index = location - 50
+        if left_index < 0:
+            left_index = 0
+
+        self.spoken_words['text'] = self.spoken_text[left_index:location]
         self.current_word_label['text'] = self.spoken_text[location:location + length]
+        self.next_words['text'] = self.spoken_text[location + length:location + length + 50]
         if self.highlight_index1 is not None:
             self.text_area.tag_remove(TAG_CURRENT_WORD, self.highlight_index1, self.highlight_index2)
         self.highlight_index1 = "1.{}".format(location)
         self.highlight_index2 = "1.{}".format(location + length)
+        self.text_area.see(self.highlight_index1)
         self.text_area.tag_add(TAG_CURRENT_WORD, self.highlight_index1, self.highlight_index2)
 
     def onEnd(self, name, completed):
         self.speak_button['state'] = NORMAL
+        self.stop_button['state'] = DISABLED
         print("onEnd")
 
     def speak(self, event):
         if self.speak_button['state'].__str__() == NORMAL:
-
             self.spoken_text = self.text_area.get("1.0", END).replace('\n', ' ')
             self.text_area.delete("1.0", END)
             self.text_area.insert(END, self.spoken_text)
